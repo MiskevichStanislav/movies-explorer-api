ae8f7ef430a36ae1768cc1448b7e931c30a7da84
Miskevich Stanislav <miskevich95@yandex.ru>
2022-07-28 04:59:33 +0300

    Deleting the react-mesto-api-full work template

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,29 +1,3 @@
-// const cors = require('cors');
-
-// const listUrl = () => {
-//   const { NODE_ENV } = process.env;
-//   let list = [];
-//   if (NODE_ENV) {
-//     list = [
-//     //   'https://msprod.nomoredomains.xyz',
-//     ];
-//   } else {
-//     list = [
-//       'http://localhost:3000',
-//       'http://localhost:3001',
-//     ];
-//   }
-//   return list;
-// };
-
-// const allowedCors = {
-//   origin: listUrl(),
-//   credentials: true,
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-// };
-
-// module.exports = cors(allowedCors);
-
 const allowedCors = [
   'https://mydiplom.nomoredomains.xyz',
   'http://localhost:3001',
